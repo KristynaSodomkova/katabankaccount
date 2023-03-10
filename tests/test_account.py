3fd5825fe5abadf35e3e7f41a71c2c818731df3d
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-10 18:35:57 +0100

    failed test to check that there is a balance attribute in class Account

--- tests/test_account.py
+++ tests/test_account.py
@@ -2,3 +2,7 @@ from katabankaccount.account import Account
 def test_account_class_exists():
     assert type(Account) == type
     assert Account.__name__ == "Account"
+
+def test_account_attribute_balance():
+    account = Account(100)
+    assert hasattr(account, 'balance')
